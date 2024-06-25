261231c9d9ece8b9cfd415f95706a311e8bcc736
Joseph DALY <joseph300905@gmail.com>
2024-06-25 12:50:06 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,18 +9,19 @@ repositories {
 }
 
 dependencies {
-    implementation("javax.annotation:javax.annotation-api:1.2")
-    implementation("com.google.code.gson:gson:2.3.1")
-    implementation("com.google.guava:guava:18.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.0.2")
-    implementation("io.netty:netty-all:4.0.56.Final")
-    implementation("org.apache.commons:commons-lang3:3.3.2")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.google.guava:guava:33.2.1-jre")
+    implementation("io.netty:netty-all:4.1.111.Final")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("com.mojang:authlib:1.5.25")
+    implementation("org.apache.logging.log4j:log4j-api:2.23.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
 }
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(22)
+        languageVersion = JavaLanguageVersion.of(8)
     }
 }
 
