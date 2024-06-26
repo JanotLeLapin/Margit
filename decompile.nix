af5f3d92dea5d2763bd0d6b0e0d2035496a61efc
Joseph DALY <joseph300905@gmail.com>
2024-06-26 14:39:41 +0200

    extract required resources

--- decompile.nix
+++ decompile.nix
@@ -10,10 +10,11 @@
   phases = [ "buildPhase" "installPhase" ];
   buildInputs = [ jdk8 unzip gnutar ];
   buildPhase = ''
-    mkdir -p classes src/main/java
+    mkdir -p classes src/main/java src/main/resources
     unzip ${margit-mapped-jar} "net/minecraft/server/*" -d classes
+    unzip ${margit-mapped-jar} "assets/*" log4j2.xml yggdrasil_session_pubkey.der -d src/main/resources
     java -jar ${margit-build-data.fernFlower} -dgs=1 -hdc=0 -rbr=0 -asc=1 -udv=0 classes src/main/java
-    tar -czvf decompiled.tar.gz src/main/java
+    tar -czvf decompiled.tar.gz src
   '';
   installPhase = ''
     mv decompiled.tar.gz $out
