bb49aeecc7d0da429773f7abe0d7d70ab7a76851
Joseph DALY <joseph300905@gmail.com>
2024-09-25 21:14:04 +0200

    add github workflow

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java-library")
+    id("maven-publish")
 }
 
 group = "io.margit.api"
@@ -10,6 +11,24 @@ repositories {
 }
 
 dependencies {
-  api("com.google.inject:guice:7.0.0")
-  annotationProcessor("com.google.inject:guice:7.0.0")
+    api("com.google.inject:guice:7.0.0")
+    annotationProcessor("com.google.inject:guice:7.0.0")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/JanotLeLapin/Margit")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("PAT_GITHUB")
+            }
+        }
+    }
 }
